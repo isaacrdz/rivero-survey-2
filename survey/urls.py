ef173d4e608e39a19200d71abe9e8ffa0981ebc8
isaacrdz <zack_cpp@hotmail.com>
2014-05-19 15:28:56 -0500

    urls

--- survey/urls.py
+++ survey/urls.py
@@ -21,11 +21,7 @@ urlpatterns = patterns('',
 
 	# Uncomment the next line to enable the admin:
 	url(r'^admin/', include(admin.site.urls)),
-	urlpatterns += patterns('',
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT,
-    }),
-)
+	
 )
 
 # media url hackery. le sigh. 
